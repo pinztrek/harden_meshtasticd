e14fb86a6f1082f8878d5037edd363cc1af33a08
Alan Barrow <traveler@pinztrek.com>
2025-07-20 23:03:08 -0400

    zram and initial apt work

--- harden.sh
+++ harden.sh
@@ -19,6 +19,9 @@ if [ -z "${DEBIAN_FRONTEND}" ]; then
     export DEBIAN_FRONTEND=noninteractive
 fi
 
+# JAB add root check, must be sudo'd
+
+
 # JAB install needed scripts, assume git clone and script started from that dir
 # usr/local
 #unpack filesystem/usr/local/sbin/ /usr/local/sbin/ root
@@ -41,8 +44,49 @@ apt remove -y iptables exim4-base exim4-config exim4-daemon-light
 apt autoremove -y
 apt purge -y exim4-base exim4-config exim4-daemon-light
 # JAB tune for meshtasticd and tools like logrotate, Log2Ram, etc
+apt install -y lunzip jq wget
 #apt install -y asl3 asl3-menu asl3-update-nodelist allmon3 asl3-pi-appliance \
 	vim-nox
+
+# Setup active dirs which do not need persistance as tmpfs
+cat - >> /etc/fstab <<EOF
+
+tmpfs	/tmp		tmpfs	defaults,noatime,nosuid,nodev,noexec,mode=1777,size=128M 0 0
+tmpfs	/var/tmp	tmpfs	defaults,noatime,nosuid,nodev,noexec,mode=1777,size=128M 0 0
+
+# Delete/comment out the next line for persistent Apache logs
+tmpfs	/var/log/apache2	tmpfs	defaults,noatime,nosuid,nodev,noexec,mode=0775,size=32M 0 0
+EOF
+
+mv /tmp /tmp.old
+mkdir -m 1777 /tmp
+mount /tmp
+mv /var/tmp /var/tmp.old
+mkdir -m 1777 /var/tmp
+mount /var/tmp
+
+
+
+
+# Get and install zram-config
+# JAB refine this to use latest automatically
+wget https://github.com/ecdye/zram-config/releases/download/v1.7.0/zram-config-v1.7.0.tar.lz
+#gh release download --repo ecdye/zram-config --pattern '*.tar.lz'
+mkdir -p zram-config && tar -xf zram-config*.tar.lz --strip-components=1 --directory=zram-config
+
+# JAB remove this once m*d installed
+mkdir -p /var/lib/portduinio
+# add dirs to ztab
+cat - >> ./zram-config/ztab <<EOF
+
+# dir   alg             mem_limit       disk_size       target_dir      bind_dir
+dir    lzo-rle         50M             150M            /var/lib/portduinio        /portduinio.bind
+EOF
+
+./zram-config/install.bash
+./zram-config/install.bash sync
+
+exit  #-------------------------------------------------------------------
 
 # Create motd & issue
 #unpack filesystem/etc /etc
@@ -63,17 +107,6 @@ rm -rf /var/log/asterisk/*
 
 AST_UID=$(getent passwd asterisk | awk -F: '{print $3}')
 AST_GID=$(getent passwd asterisk | awk -F: '{print $4}')
-cat - >> /etc/fstab <<EOF
-
-tmpfs	/tmp		tmpfs	defaults,noatime,nosuid,nodev,noexec,mode=1777,size=128M 0 0
-tmpfs	/var/tmp	tmpfs	defaults,noatime,nosuid,nodev,noexec,mode=1777,size=128M 0 0
-
-# Delete/comment out the next line for persistent Apache logs
-tmpfs	/var/log/apache2	tmpfs	defaults,noatime,nosuid,nodev,noexec,mode=0775,size=32M 0 0
-
-# Delete/comment out the next line for persistent Asterisk logs
-tmpfs	/var/log/asterisk	tmpfs	defaults,noatime,nosuid,nodev,noexec,mode=0775,size=32M,uid=${AST_UID},gid=${AST_GID} 0 0 
-EOF
 
 # Disable bluetooth for GPIO accessibility
 echo "dtoverlay=disable-bt" >> /boot/firmware/config.txt
